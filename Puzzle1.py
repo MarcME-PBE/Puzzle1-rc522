2bad25597416dc5c6d04532c832838b2f8326730
MarcME-PBE <marc.munoz.estruga@estudiantat.upc.edu>
2024-10-07 12:10:58 +0200

    Puzzle1.py modificat per mostrar UID amb 8 xifres hexadecimals

--- Puzzle1.py
+++ Puzzle1.py
@@ -1,11 +1,32 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
-reader = SimpleMFRC522() 
+class Rfid:
+    def __init__(self):
+        # Configurar el lector RC522
+        self.reader = SimpleMFRC522()
 
-try:
-    print("Acosta la teva targeta d'identificacio...") 
-    id, text = reader.read() #llegeix la tarja i retorna el UID
-    print(f"UID: {id}") 
-finally:
-    GPIO.cleanup() #neteja la configuracio dels pins GPIO despres d'executar el codi
+    def read_uid(self):
+        try:
+            # Llegir la targeta
+            print("Aproximi la targeta al lector...")
+            id, text = self.reader.read()
+            
+            # Convertir el UID a hexadecimal
+            uid_hex = hex(id)[2:].upper()  # treure el '0x' i convertir a majuscules
+            uid_hex = uid_hex.zfill(8)[:8] #omplir amb 0 si s'escau i truncar als 8 digits
+            return uid_hex
+        finally:
+            GPIO.cleanup() #neteja la configuracio dels pins GPIO despres d'executar el codi
+
+if __name__ == "__main__":
+    rf = Rfid()
+    uid = rf.read_uid()
+    if uid:
+        print(f"UID de la targeta: {uid}")
+    else:
+        print("No es pot llegir el UID.")
+        
+        
+        
+        
